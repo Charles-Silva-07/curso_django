79e225c4d2bdf1d9aef7d54459dde0aaae39dc15
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-02-16 20:24:39 -0300

    Criação da primeira app

--- pypro/urls.py
+++ pypro/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pypro.base.views
+from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
