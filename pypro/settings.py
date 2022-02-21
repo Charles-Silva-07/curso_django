22f2127f0cc5a0acf21df45b1c68f0768d2ef926
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-02-21 09:29:20 -0300

    Corrido nome procfile para Procfile com p maiusculo

--- pypro/settings.py
+++ pypro/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-default_db_url= 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
 
 DATABASES = {
-   'default': config('DATABASE_URL', default = default_db_url, cast=parse_database )
+   'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
 
 
