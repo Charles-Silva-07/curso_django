2d5b78699bbbeefd25c8fe3bfe2a824634055c77
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-02-22 12:51:07 -0300

    inserido html no projeto

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
---
+++ pypro/django_assertions.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+_test_case = TestCase()
+
+assert_contains = _test_case.assertContains
+
+
+
+
