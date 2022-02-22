17b5894da92ce2626c25572013b248cda86818e5
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-02-22 18:06:14 -0300

    Urls Nomeadas

--- pypro/urls.py
+++ pypro/urls.py
@@ -13,18 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+
 from pypro.base.views import home
-from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('home', home, name='home'),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns.append(
         path('__debug__/', include(debug_toolbar.urls))
     )
