582d7b974234f3fcbd01479365e7553cb7123b3f
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-04 09:39:59 -0300

    Correção flake8 no projeto

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    videos={
-        'motivacao':{'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 58479},
-        'instalacao-windows':{'titulo': 'Instalação windows', 'vimeo_id': 58625},
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 58479},
+        'instalacao-windows': {'titulo': 'Instalação windows', 'vimeo_id': 58625},
     }
     video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
-
