52e6ba4ad897e72544ef11f4ec91b809459ae104
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-04 09:34:41 -0300

    Utilizando Filtros no Template

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos={
+        'motivacao':{'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 58479},
+        'instalacao-windows':{'titulo': 'Instalação windows', 'vimeo_id': 58625},
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
+
