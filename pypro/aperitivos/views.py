a78af0ec0e93467b6b33db52f97985c93a9f14e4
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-04 12:29:40 -0300

    Lista de Elementos com For

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+videos = [
+     {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 58479},
+     {'slug': 'instalacao-windows', 'titulo': 'Instalação windows', 'vimeo_id': 58625},
+]
+
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 58479},
-        'instalacao-windows': {'titulo': 'Instalação windows', 'vimeo_id': 58625},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
