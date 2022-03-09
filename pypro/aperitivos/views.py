1d8bb9044e030d77b0aeefc2376592f13c486fdc
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-09 17:28:52 -0300

    Criação de Modelos nos Teste_02

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -18,25 +18,3 @@ def indice(request):
 def video(request, slug):
     video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
