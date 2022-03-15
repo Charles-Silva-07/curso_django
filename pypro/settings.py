a9073a112afb36a745bf646efea77a83eb6b3ef1
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-15 19:59:48 -0300

    Reaproveitando Apps de Terceiros

--- pypro/settings.py
+++ pypro/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pypro.base',
     'pypro.aperitivos',
-    'pypro.modulos'
+    'pypro.modulos',
+    'ordered_model',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'pypro.modulos.context_processors.listar_modulos'
+                'pypro.modulos.context_processors.listar_modulos',
             ],
         },
     },
