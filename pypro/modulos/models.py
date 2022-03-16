2edd2fe1c1e3b5f62c6d3bb417eef881f8bd3ddf
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-16 09:59:18 -0300

    Migração de Dados

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -6,9 +6,15 @@ class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
     publico = models.TextField()
     descricao = models.TextField()
+    slug = models.SlugField(unique=True)
 
     class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
+
