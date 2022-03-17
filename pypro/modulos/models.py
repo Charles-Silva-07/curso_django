b9b40a9f4a05b1c92b6c20de0d47309794976f32
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-16 23:44:21 -0300

    Mapeamento de url de detalhamento de aula pc casa ok

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -17,4 +17,3 @@ class Modulo(OrderedModel):
 
     def get_absolute_url(self):
         return reverse('modulos:detalhe', kwargs={'slug': self.slug})
-
--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -5,4 +5,3 @@ from pypro.modulos import facade
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
-
