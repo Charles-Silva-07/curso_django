85f6ec0d97d204d8e3290749a1e741acc94cf5eb
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-16 23:41:48 -0300

    Mapeamento de url de detalhamento de aula pc casa

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -7,3 +7,7 @@ def listar_modulos_ordenados() -> List[Modulo]:
     Lista módulos ordenados por títulos
     """
     return list(Modulo.objects.order_by('order').all())
+
+
+def encontrar_modulo(slug: str) -> Modulo:
+    return Modulo.objects.get(slug=slug)
