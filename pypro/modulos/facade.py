9ea2cb17f6f04718f8a0b74e18d7f1a10b54e432
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-21 12:54:39 -0300

    Resolvendo problemas no html

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,5 +1,5 @@
 from typing import List
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 def listar_modulos_ordenados() -> List[Modulo]:
@@ -15,3 +15,30 @@ def encontrar_modulo(slug: str) -> Modulo:
 
 def listar_aulas_de_modulo_ordenados(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
+
+
+def encontrar_aula(slug):
+    return Aula.objects.get(slug=slug)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
