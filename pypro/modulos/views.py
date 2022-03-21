fbf84d02b88e4a942f7c4fbd2e090d142ae5dedb
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-21 09:28:45 -0300

    Resolvendo problema de travamento na aula

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -6,4 +6,8 @@ from pypro.modulos import facade
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_modulo_ordenados(modulo)
-    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas':aulas})
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    pass
