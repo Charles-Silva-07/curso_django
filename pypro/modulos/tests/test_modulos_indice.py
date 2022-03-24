455be6ac16d919fbc04766465f4702b9bdee419f
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-24 16:37:58 -0300

    Criação de página de índice de aulas

---
+++ pypro/modulos/tests/test_modulos_indice.py
@@ -0,0 +1,50 @@
+import pytest
+from django.urls import reverse
+from pypro.django_assertions import assert_contains
+from model_bakery import baker
+from pypro.modulos.models import Modulo
+from pypro.modulos.models import Aula
+
+
+@pytest.fixture
+def modulos(db):
+    return baker.make(Modulo, 2)
+
+
+@pytest.fixture
+def aulas(modulos):
+    aulas=[]
+    for modulo in modulos:
+        aulas.extend(baker.make(Aula, 3, modulo=modulo))
+    return aulas
+
+
+@pytest.fixture
+def resp(client, modulos, aulas):
+    resp = client.get(reverse('modulos:indice'))
+    return resp
+
+
+def test_indice_disponivel(resp):
+    assert resp.status_code == 200
+
+# def test_titulo(resp, modulo: Modulo):
+#     assert_contains(resp, modulo.titulo)
+#
+#
+# def test_descricao(resp, modulo: Modulo):
+#     assert_contains(resp, modulo.descricao)
+#
+#
+# def test_publico(resp, modulo: Modulo):
+#     assert_contains(resp, modulo.publico)
+#
+#
+# def test_aulas_titulos(resp, aulas):
+#     for aula in aulas:
+#         assert_contains(resp, aula.titulo)
+
+#
+# def test_aulas_links(resp, aulas):
+#     for aula in aulas:
+#         assert_contains(resp, aula.get_absolute_url())
