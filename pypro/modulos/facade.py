3061ebc96b82fbd04faf40b2f026ba7f5aad6ffd
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-24 23:53:12 -0300

    Método prefetch_related ok

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -23,28 +23,3 @@ def encontrar_aula(slug):
 
 def listar_modulos_com_aulas():
     return Modulo.objects.order_by('order').prefetch_related('aula_set').all()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
