a5bf3fcb0d7bfac5d952afcb6aa567bc29140af3
Charles-Silva-07 <charllesrocklp@gmail.com>
2022-03-24 23:51:51 -0300

    implementado o Método prefetch_related

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -13,9 +13,38 @@ def encontrar_modulo(slug: str) -> Modulo:
     return Modulo.objects.get(slug=slug)
 
 
-def listar_aulas_de_modulo_ordenados(modulo: Modulo):
+def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
 
 
 def encontrar_aula(slug):
     return Aula.objects.select_related('modulo').get(slug=slug)
+
+
+def listar_modulos_com_aulas():
+    return Modulo.objects.order_by('order').prefetch_related('aula_set').all()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pypro/modulos/tests/test_modulos_indice.py
+++ pypro/modulos/tests/test_modulos_indice.py
@@ -1,9 +1,11 @@
 import pytest
 from django.urls import reverse
-from pypro.django_assertions import assert_contains
 from model_bakery import baker
-from pypro.modulos.models import Modulo
-from pypro.modulos.models import Aula
+
+from pypro.django_assertions import assert_contains
+from pypro.modulos.models import Aula, Modulo
+
+from typing import List
 
 
 @pytest.fixture
@@ -13,7 +15,7 @@ def modulos(db):
 
 @pytest.fixture
 def aulas(modulos):
-    aulas=[]
+    aulas = []
     for modulo in modulos:
         aulas.extend(baker.make(Aula, 3, modulo=modulo))
     return aulas
@@ -28,23 +30,27 @@ def resp(client, modulos, aulas):
 def test_indice_disponivel(resp):
     assert resp.status_code == 200
 
-# def test_titulo(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.titulo)
-#
-#
-# def test_descricao(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.descricao)
-#
-#
-# def test_publico(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.publico)
-#
-#
-# def test_aulas_titulos(resp, aulas):
-#     for aula in aulas:
-#         assert_contains(resp, aula.titulo)
 
-#
-# def test_aulas_links(resp, aulas):
-#     for aula in aulas:
-#         assert_contains(resp, aula.get_absolute_url())
+def test_titulo(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.titulo)
+
+
+def test_descricao(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.descricao)
+
+
+def test_publico(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.publico)
+
+
+def test_aulas_titulos(resp, aulas: List[Aula]):
+    for aula in aulas:
+        assert_contains(resp, aula.titulo)
+
+
+def test_aulas_urls(resp, aulas: List[Aula]):
+    for aula in aulas:
+        assert_contains(resp, aula.get_absolute_url())
